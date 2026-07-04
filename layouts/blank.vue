62b8acd7b9432f030bd0443619eb889fb6224752
anthonyanderson <tonyanderson526@gmail.com>
2026-07-03 22:47:10 -0400

    full push

--- layouts/blank.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="w-full layout-wrapper bg-transparent-safe">
-    <Nuxt />
-  </div>
-</template>
-
-<script>
-export default {}
-</script>
--- plugins/capacitor/dynamicColor.js
+++
@@ -1,8 +0,0 @@
-import { registerPlugin } from '@capacitor/core'
-
-const DynamicColor = registerPlugin('DynamicColor', {
-  web: () => import('./dynamicColor.web.js').then((m) => new m.DynamicColorWeb())
-})
-
-export * from './dynamicColor.definitions.js'
-export { DynamicColor }
